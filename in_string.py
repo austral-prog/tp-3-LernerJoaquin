eb0045bf97c10b6ed986c8745bf5fde1b0c8f254
LernerJoaquin <lernerjoaquin@gmail.com>
2025-04-10 12:02:36 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,5 +1,5 @@
 def check_vowels():
-    name= input("introducir el nombre: ")
+    name= input("introducir el nombre:")
     name= name.lower()
     print("contiene a:", "a" in name)
     print("contiene e:", "e" in name)
