5aa5ac15c44de43b6305b7bd58ea4c0c8ac67f55
LernerJoaquin <lernerjoaquin@gmail.com>
2025-07-12 18:13:39 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,8 +1,11 @@
-def slice_simple():
-    texto= "Awesome"
-    texto = texto.lower()
-    print(texto[0:3])
-    print(texto[0:5])
-    print(texto[0:4] + texto[-3:])
-slice_simple()
-    
+texto = input("Ingresá una palabra: ")
+texto = texto.lower()
+primeras_3 = texto[0:3]
+mitad = len(texto) // 2
+inicio_medio = mitad - 1
+letras_medio = texto[inicio_medio:inicio_medio + 3]
+prim_a_cuarta = texto[0:4]
+ultimas_3 = texto[-3:]
+print(primeras_3)
+print(letras_medio)
+print(prim_a_cuarta + ultimas_3)    
