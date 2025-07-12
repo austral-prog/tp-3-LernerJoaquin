ef97f95e5a5a1243e5f6dcacd00240164fb1c3ed
LernerJoaquin <lernerjoaquin@gmail.com>
2025-07-12 18:24:33 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,8 +1,9 @@
-nombre = input("Ingrese un nombre: ")
-minúsculas
-nombre = nombre.lower()
-print("Contiene a:", 'a' in nombre)
-print("Contiene e:", 'e' in nombre)
-print("Contiene i:", 'i' in nombre)
-print("Contiene o:", 'o' in nombre)
-print("Contiene u:", 'u' in nombre)
+def check_vowels():
+    name=input("")
+    print("Contiene a:", "a" in name.lower())
+    print("Contiene e:", "e" in name.lower())
+    print("Contiene i:", "i" in name.lower())
+    print("Contiene o:", "o" in name.lower())
+    print("Contiene u:", "u" in name.lower())
+
+   
