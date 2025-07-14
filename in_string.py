5837ccb0e23c430fa1a7ab4b761bb0dd235257da
LernerJoaquin <lernerjoaquin@gmail.com>
2025-07-14 18:47:22 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,12 +1,12 @@
 def check_vowels():
     # Ingresar el nombre
     nombre = input("Ingrese un nombre: ")
-    # Convertir a minúsculas para evitar errores con mayúsculas
+    # Convertir a minúsculas
     nombre = nombre.lower()
 
-# Verificar presencia de cada vocal (sin tildes)
-print("Contiene a:", "a" in nombre)
-print("Contiene e:", "e" in nombre)
-print("Contiene i:", "i" in nombre)
-print("Contiene o:", "o" in nombre)
-print("Contiene u:", "u" in nombre)
+    # Verificar presencia de cada vocal
+    print("Contiene a:", "a" in nombre)
+    print("Contiene e:", "e" in nombre)
+    print("Contiene i:", "i" in nombre)
+    print("Contiene o:", "o" in nombre)
+    print("Contiene u:", "u" in nombre)
