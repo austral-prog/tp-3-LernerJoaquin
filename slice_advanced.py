433873b1de0792acf844c84d69af69ca70ba4bc3
LernerJoaquin <lernerjoaquin@gmail.com>
2025-07-14 18:49:02 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,3 +1,8 @@
 def slice_advanced():
-  texto=input("Escriba su oración:") 
-  print(texto[4: :2])
+    texto = input("Escriba su oración: ")
+
+    if len(texto) < 5:
+        print("El texto debe tener al menos 5 caracteres.")
+    else:
+        resultado = texto[4::2]
+        print("Resultado:", resultado)
