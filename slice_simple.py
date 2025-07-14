09c6cb703148ba7e8b49e95c31333d879a13178a
LernerJoaquin <lernerjoaquin@gmail.com>
2025-07-14 20:50:41 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,22 +1,6 @@
 def slice_simple():
-    texto = input("Ingrese un texto: ").lower()
-
-    # Primeras 3 letras
-    primeras_3 = texto[0:3]
-
-    # Las 3 letras del medio
-    longitud = len(texto)
-    medio = longitud // 2
-    if longitud >= 3:
-        tres_del_medio = texto[medio - 1 : medio + 2]
-    else:
-        tres_del_medio = texto 
-
-    primeras_4 = texto[0:4]
-
-    antepenultima_a_ultima = texto[len(texto)-3:]
-
-    print("Primeras 3 letras:", primeras_3)
-    print("3 letras del medio:", tres_del_medio)
-    print("De la primera a la cuarta:", primeras_4)
-    print("De la antepenúltima a la última:", antepenultima_a_ultima)
+    texto= "Awesome"
+    texto = texto.lower()
+    print(texto[0:3])
+    print(texto[2:5])
+    print(texto[0:4] + texto[-3:])
