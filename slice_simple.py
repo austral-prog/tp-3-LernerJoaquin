9ac476822461912557e1620e36096ea3e1d5717e
LernerJoaquin <lernerjoaquin@gmail.com>
2025-07-14 18:49:47 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -10,15 +10,12 @@ def slice_simple():
     if longitud >= 3:
         tres_del_medio = texto[medio - 1 : medio + 2]
     else:
-        tres_del_medio = texto  # en caso de texto muy corto
+        tres_del_medio = texto 
 
-    # De la primera a la cuarta letra (incluida)
     primeras_4 = texto[0:4]
 
-    # De la antepenúltima a la última letra (usando índices positivos)
     antepenultima_a_ultima = texto[len(texto)-3:]
 
-    # Mostrar resultados
     print("Primeras 3 letras:", primeras_3)
     print("3 letras del medio:", tres_del_medio)
     print("De la primera a la cuarta:", primeras_4)
